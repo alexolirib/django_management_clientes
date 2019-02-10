3b0c462a68c4264101818208e8c40e823da25cbe
Alexandre Ribeiro <alexolirib@gmail.com>
2019-02-10 17:23:47 -0200

    finalizado todo o caminho do login até logout

--- django_management_clientes/urls.py
+++ django_management_clientes/urls.py
@@ -5,13 +5,15 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 from client import urls as clients_urls
-
+from home import urls as home_urls
 
 urlpatterns = [
     path('client/', include(clients_urls)),
+    path('', include(home_urls)),
     #login - django já prover toda a parte de segurança do app
     path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    #para personalzar o logout vou fazer isso no app Home
+    #path('logout/', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
